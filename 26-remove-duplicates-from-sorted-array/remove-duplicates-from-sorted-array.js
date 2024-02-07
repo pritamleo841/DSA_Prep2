deeb3e1cda2b5c37acde1c71dc1c31c582a613a3
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-07 19:56:54 +0530

    Time: 58 ms (83.39%) | Memory: 52.5 MB (11.07%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,23 +3,33 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let uniq=nums[0];
-    let count=1;
+    //Crisp thinking - good coding
+    let left=1;
     for(let i=1;i<nums.length;i++){
-        if(nums[i]==='_')return count;
-        if(nums[i]===uniq){
-            let id=i;
-            while(id<nums.length-1){
-                if(nums[id+1]==='_')break;
-                nums[id]=nums[id+1];
-                id++;
-            }
-            nums[id]='_';
-            i--;
-        }
-        else{
-            uniq=nums[i];
-            count++;
+        if(nums[i]!=nums[i-1]){
+            nums[left]=nums[i];
+            left++;
         }
     }
+    return left;
+    //Passed - but took very much time(1749 ms)
+    // let uniq=nums[0];
+    // let count=1;
+    // for(let i=1;i<nums.length;i++){
+    //     if(nums[i]==='_')return count;
+    //     if(nums[i]===uniq){
+    //         let id=i;
+    //         while(id<nums.length-1){
+    //             if(nums[id+1]==='_')break;
+    //             nums[id]=nums[id+1];
+    //             id++;
+    //         }
+    //         nums[id]='_';
+    //         i--;
+    //     }
+    //     else{
+    //         uniq=nums[i];
+    //         count++;
+    //     }
+    // }
 };
