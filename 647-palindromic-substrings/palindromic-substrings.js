471fba5a219c39dd59421432f4f747f0b5471d7e
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-10 16:20:43 +0530

    Time: 61 ms (70.12%) | Memory: 49.6 MB (46.58%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    //Solution includes check for  odd length and even length 
+    //palindromes and counting from start postion to end postion
+    //by using EXAPANSION FROM MIDDLE Concept : O(n*n)
+    let totalCount = 0;
+
+    const countPalindromes = (l,r)=>{
+        let res=0;
+        while(l>=0 && r<=s.length && s[l]==s[r]){
+            res++;
+            l-=1;
+            r+=1;
+        }
+        return res;
+    };
+
+    for(let i=0;i<s.length;i++){
+        //odd length palindromes
+        totalCount+=countPalindromes(i,i);
+        //even length palindromes
+        totalCount+=countPalindromes(i,i+1);
+    }
+    return totalCount;
+    //LEARN MANACHER'S ALGORITHM to find solution in O(n) time
+};
