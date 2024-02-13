057e30986323bc5c80017cb9637eaf3678932b94
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-13 18:27:45 +0530

    Time: 61 ms (22.01%) | Memory: 49.2 MB (11.30%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -6,8 +6,11 @@
 var strStr = function(haystack, needle) {
     for(let i=0;;i++){
         for(let j=0;;j++){
+            //If needle is traversed
             if(j===needle.length)return i;
+            //If exceeded haystack length
             if(i+j===haystack.length)return -1;
+            //If at any time, both strings don't match
             if(needle[j]!=haystack[i+j])break;
         }
     }
