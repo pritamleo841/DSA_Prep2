cad122d71d25a5a344eeab08e91332a8b219a276
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-14 18:23:46 +0530

    Time: 346 ms (17.48%) | Memory: 94.3 MB (67.28%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    let positive=0,negetive=1;
+    let result=new Array(nums.length).fill(0);
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>=0){
+            result[positive]=nums[i];
+            positive+=2;
+        }
+        else{
+            result[negetive]=nums[i];
+            negetive+=2;
+        }
+    }
+    return result;
+};
