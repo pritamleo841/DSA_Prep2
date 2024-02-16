3ea97e0500b1cdbfa6c07d46db6694dd76c37bf2
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-16 20:03:05 +0530

    Time: 199 ms (28.21%) | Memory: 86.2 MB (23.59%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.js
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.js
@@ -11,20 +11,28 @@ var findLeastNumOfUniqueInts = function(arr, k) {
     for(let n of arr){
         map.set(n,(map.get(n) || 0)+1);
     }
-    let sarr=Array.from(map.values());
-    let count=0;
-    sarr.sort((a,b)=>a-b);
-    for(let i=0;i<sarr.length;i++){
-        if(k>sarr[i]){
-            k=k-sarr[i];
-            sarr[i]=0;
-        }
-        else{
-            sarr[i]=sarr[i]-k;
-            k=0;
-        }
-        if(sarr[i]!=0)count++;
+    // let sarr=Array.from(map.values());
+    // let count=0;
+    // sarr.sort((a,b)=>a-b);
+    // for(let i=0;i<sarr.length;i++){
+    //     if(k>sarr[i]){
+    //         k=k-sarr[i];
+    //         sarr[i]=0;
+    //     }
+    //     else{
+    //         sarr[i]=sarr[i]-k;
+    //         k=0;
+    //     }
+    //     if(sarr[i]!=0)count++;
+    // }
+    // return count;
+    //Using heap
+    let pq = new MinPriorityQueue({ priority: x => x.freq });
+    for (const [k, v] of map) {
+        pq.enqueue({ freq: v });
     }
-    return count;
-    
+    while (!pq.isEmpty() && k >= pq.front().element.freq) {
+        k -= pq.dequeue().element.freq;
+    }
+    return pq.size();
 };
