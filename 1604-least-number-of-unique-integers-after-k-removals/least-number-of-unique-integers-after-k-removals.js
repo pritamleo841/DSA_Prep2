ea9fe84a04c16140767e6b7253e318fea1d4f129
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-16 20:01:39 +0530

    Time: 122 ms (68.72%) | Memory: 72.4 MB (62.05%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function(arr, k) {
+    //sort the numbers by frequency
+    //remove 1 elements from each upto k
+    //find total unique elements
+    let map = new Map();
+    for(let n of arr){
+        map.set(n,(map.get(n) || 0)+1);
+    }
+    let sarr=Array.from(map.values());
+    let count=0;
+    sarr.sort((a,b)=>a-b);
+    for(let i=0;i<sarr.length;i++){
+        if(k>sarr[i]){
+            k=k-sarr[i];
+            sarr[i]=0;
+        }
+        else{
+            sarr[i]=sarr[i]-k;
+            k=0;
+        }
+        if(sarr[i]!=0)count++;
+    }
+    return count;
+    
+};
