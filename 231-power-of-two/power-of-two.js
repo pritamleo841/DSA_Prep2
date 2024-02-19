30a17e47f634b2858266e7be75b2b8da4b78fe03
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-19 12:02:31 +0530

    Time: 63 ms (77.05%) | Memory: 53.4 MB (17.88%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -4,6 +4,11 @@
  */
 var isPowerOfTwo = function(n) {
     return ((n>0 && (n&(n-1)))===0);
+    /**
+    if(!n)return false;
+    if(n==1)return true;
+    return isPowerOfTwo(n/2) and (n%2==0);
+    */
 };
 /***
 n=8=>1000
