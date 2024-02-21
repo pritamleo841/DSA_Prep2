5ff1b5fac6970491dc5ba00ccc5d2b9eead99aa1
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-21 19:57:00 +0530

    Time: 52 ms (57.24%) | Memory: 48.3 MB (46.00%) - LeetSync

---
+++ 290-word-pattern/word-pattern.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+   const newMap = (str1,str2)=>{
+       let [map1,map2] =[new Map(),new Map()];
+       let i=0;
+       while(i<str1.length){
+           map1.set(str1[i],str2[i]);
+           map2.set(str2[i],str1[i]);
+           i++;
+       }
+       return [map1,map2];
+   };
+
+   let sarray = s.split(' ');
+   if(pattern.length!=sarray.length)return false;
+   let [map1,map2] = newMap(pattern,sarray);
+   if(map1.size!=map2.size)return false;
+   let i=0;
+   while(i<map1.size){
+       if(
+           (map1.get(pattern[i])!=sarray[i]) ||
+           (map2.get(sarray[i])!=pattern[i])
+       )return false;
+       i++;
+   }
+   return true;
+
+
+};
