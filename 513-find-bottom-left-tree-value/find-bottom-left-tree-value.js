a73780775d2163ef5f04f67cf1f843c9a54c9ff3
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-28 20:18:05 +0530

    Time: 62 ms (67.84%) | Memory: 54.1 MB (18.13%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.js
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.js
@@ -12,15 +12,13 @@
  */
 var findBottomLeftValue = function(root) {
     //If we traverse level order, last remaining value will be BottomLeftValue
-    let prevVal=root;
+    let curr=root;
     let queue=[];
     queue.push(root);
     while(queue.length!=0){
-        let curr = queue.shift();
-        prevVal=curr;
+        curr = queue.shift();
         if(curr.right!=null)queue.push(curr.right);
         if(curr.left!=null)queue.push(curr.left);
     }
-    console.log(prevVal.val);
-    return prevVal.val;
+    return curr.val;
 };
