07bae559844b3b8027deffe85b0d0d3d23a60cc0
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-08 19:34:09 +0530

    Time: 63 ms (42.76%) | Memory: 52 MB (18.06%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.js
+++ 101-symmetric-tree/symmetric-tree.js
@@ -21,3 +21,15 @@ var isSame = function(lroot,rroot){
     }
     return true;
 };
+/**
+const isSym = (node1, node2) => {
+    if (node1 === null && node2 === null) return true;
+    if (node1 === null || node2 === null) return false;
+    return node1.val === node2.val && isSym(node1.left, node2.right) && isSym(node1.right, node2.left);
+};
+
+var isSymmetric = function(root) {
+    return isSym(root.left, root.right);
+};
+
+ */
