107e0024ae778045aed9778dc58edec5efe9ec68
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-08 19:17:53 +0530

    Time: 69 ms (25.26%) | Memory: 51.4 MB (33.59%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
@@ -8,12 +8,8 @@ var maxFrequencyElements = function(nums) {
     let totalFreq = 0;
     for(let n of nums){
         map.set(n,(map.get(n) || 0)+1);
-    }
-    console.log(map);
-    for(let n of nums){
         max_count=Math.max(map.get(n),max_count);
     }
-    console.log(max_count);
     for(let n of nums){
         if(map.get(n)===max_count){
             totalFreq++;
