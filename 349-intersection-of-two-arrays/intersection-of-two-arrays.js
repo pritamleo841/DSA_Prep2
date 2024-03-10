61f328cc7941b1ff4804d2eb41b21ed77da4bf60
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-10 08:02:51 +0530

    Time: 56 ms (55.14%) | Memory: 51.4 MB (10.01%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,17 +4,17 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let res = [];
+    let res = new Set();
     let set = new Set();
     for(let n of nums1)set.add(n);
     for(let n of nums2){
-        if(set.has(n))res.push(n);
+        if(set.has(n))res.add(n);
     }
-    let set1 = new Set();
-    for(let n of res){
-        set1.add(n);
-    }
-    res.length=0;
-    for(let val of set1)res.push(val)
-    return res;
+    // let set1 = new Set();
+    // for(let n of res){
+    //     set1.add(n);
+    // }
+    // res.length=0;
+    // for(let val of set1)res.push(val)
+    return Array.from(res);
 };
