e34a3be68468de0bd9027315d09eba58107023e8
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-12 20:39:17 +0530

    Time: 65 ms (68.31%) | Memory: 52.7 MB (33.96%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
@@ -10,25 +10,38 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
-var flatten = function(root) {
-    //Using two stack
-    let stack=[root];
-    let waiting=[];
-    while(stack.length){
-        const node=stack.pop();
-        if(!node)continue;
-        if(node.left){
-            if(node.right)waiting.push(node.right);
-            node.right=node.left;
-            node.left=null;
+var flatten = function (root) {
+    // //Using two stack
+    // let stack=[root];
+    // let waiting=[];
+    // while(stack.length){
+    //     const node=stack.pop();
+    //     if(!node)continue;
+    //     if(node.left){
+    //         if(node.right)waiting.push(node.right);
+    //         node.right=node.left;
+    //         node.left=null;
+    //     }
+    //     if(node.right){
+    //         stack.push(node.right);
+    //         continue;
+    //     }
+    //     if(waiting.length){
+    //         node.right=waiting.pop();
+    //         stack.push(node.right);
+    //     }
+    // }
+    let curr = root
+    while (curr) {
+        if (curr.left) {
+            //
+            let runner = curr.left
+            while (runner.right) runner = runner.right
+            // the right sub tree gets move to underneath the runner, and then that left tree gets moved
+            // to the right, then the left is cleared. you continue down the right
+            // and absorb all left subtrees
+            runner.right = curr.right, curr.right = curr.left, curr.left = null
         }
-        if(node.right){
-            stack.push(node.right);
-            continue;
-        }
-        if(waiting.length){
-            node.right=waiting.pop();
-            stack.push(node.right);
-        }
+        curr = curr.right
     }
 };
