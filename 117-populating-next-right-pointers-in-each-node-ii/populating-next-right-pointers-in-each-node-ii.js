f57bf83268bc899fdfe786edc848b009809fbf2d
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-12 20:12:18 +0530

    Time: 70 ms (44.52%) | Memory: 53.4 MB (40.64%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.js
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.js
@@ -13,46 +13,29 @@
  * @return {Node}
  */
 var connect = function (root) {
-    let curr = root;
-    while (curr != null) {
-        let start = null; // (1)
-        let prev = null;
-        while (curr != null) { // (2)
-            if(start == null) { // (3)
-                if (curr.left) start = curr.left;
-                else if (curr.right) start = curr.right;
-                prev = start; // (4)
-            }
-            if (prev != null) {
-                if (curr.left && prev != curr.left) {
-                    prev = prev.next = curr.left; // (5)
-                }
-                if (curr.right && prev != curr.right) {
-                    prev = prev.next = curr.right;
-                }
-            }
-            curr = curr.next; // (6)
+    if(!root) return root; 
+    
+    root.depth = 0; //added a depth key to root
+    let queue = [root];
+
+    while(queue.length) {
+        const node = queue.shift(); //bfs approach
+        if(node.left) {
+            node.left.depth = node.depth + 1;
+            queue.push(node.left);
         }
-        curr = start; // (7)
+
+        if(node.right) {
+            node.right.depth = node.depth + 1;
+            queue.push(node.right);
+        }
+
+        if(queue[0] && queue[0].depth === node.depth) {
+            node.next = queue[0];
+        } else {
+            node.next = null;
+        }
     }
+
     return root;
 };
-/*
-Additional Comments:
-
-(1) Used as a marker for the beginning of the next level
-
-(2) Traverse nodes left to right at current level until there are no more nodes to traverse
-
-(3) We have not found the beginning of the next level
-
-(4) If both curr.left and curr.right are null, then start will stay null and prev will also stay null
-
-(5) JavaScript operator associativity for the equal sign (=) is right to left. 
-	So first we set prev.next to curr.left and then re-assign prev to prev.next which is curr.left
-
-(6) Move to the next node
-
-(7) Traverse down to the next level
-	
-*/
