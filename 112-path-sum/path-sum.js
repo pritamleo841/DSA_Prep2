194227aa957de35dd4e0d9d0e4ac00c852bc1ffe
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-13 20:05:40 +0530

    Time: 68 ms (23.53%) | Memory: 51.3 MB (79.36%) - LeetSync

---
+++ 112-path-sum/path-sum.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    const dfs=(node,target)=>{
+        if(!node)return false;
+        if(!node.left && !node.right){
+            return target-node.val==0;
+        }
+        return dfs(node.left,target-node.val) || dfs(node.right,target-node.val);
+    };
+    return dfs(root,targetSum);
+};
