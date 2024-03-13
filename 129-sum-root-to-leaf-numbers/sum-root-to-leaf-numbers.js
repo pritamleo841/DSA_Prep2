617babcc31c67d00d73a9d5851d7ca12deed4cd5
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-13 20:36:26 +0530

    Time: 49 ms (86.41%) | Memory: 49.3 MB (80.45%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
@@ -11,24 +11,36 @@
  * @return {number}
  */
 var sumNumbers = function(root) {
-    let res=0;
-    const dfs=(node,arr)=>{
-        if(!node.left && !node.right){
-            res+=Number(arr.join(''));
-            return ;
+    // let res=0;
+    // const dfs=(node,arr)=>{
+    //     if(!node.left && !node.right){
+    //         res+=Number(arr.join(''));
+    //         return ;
+    //     }
+    //     if(node.left){
+    //         arr.push(node.left.val);
+    //         dfs(node.left,arr);
+    //         arr.pop();
+    //     }
+    //     if(node.right){
+    //         arr.push(node.right.val);
+    //         dfs(node.right,arr);
+    //         arr.pop();
+    //     }
+    // }
+    // dfs(root,[root.val]);
+    // return res;
+
+    let ret = 0;
+    const dfs = (node, val) => {
+        val = (val * 10) + node.val;
+        if (!node.left && !node.right) {
+            ret += val;
+            return;
         }
-        if(node.left){
-            arr.push(node.left.val);
-            dfs(node.left,arr);
-            arr.pop();
-        }
-        if(node.right){
-            arr.push(node.right.val);
-            dfs(node.right,arr);
-            arr.pop();
-        }
+        if(node.left)dfs(node.left, val);
+        if(node.right)dfs(node.right, val);
     }
-    dfs(root,[root.val]);
-    console.log(res);
-    return res;
+    dfs(root, 0);
+    return ret;
 };
