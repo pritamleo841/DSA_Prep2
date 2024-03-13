c5528b3b7289b60363cbaef8764d45e1e128c31f
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-13 19:48:55 +0530

    Time: 55 ms (68.21%) | Memory: 48.9 MB (55.90%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.js
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.js
@@ -5,9 +5,10 @@
 var pivotInteger = function(n) {
     //O(1) space
     let prefixsum=0,postfixsum=0;
-    for(let i=1;i<=n;i++){
-        prefixsum+=i;
-    }
+    // for(let i=1;i<=n;i++){
+    //     prefixsum+=i;
+    // }
+    prefixsum=(n*(n+1)/2);
     for(let i=n;i>=1;i--){
         postfixsum+=i;
         if(prefixsum===postfixsum)return i;
