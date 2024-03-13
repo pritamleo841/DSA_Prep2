0632af99419972ab7f7a6857bcd04df71ce1caee
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-13 19:48:03 +0530

    Time: 60 ms (47.69%) | Memory: 48.7 MB (74.36%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.js
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var pivotInteger = function(n) {
-    let prefix=[],postfix=[];
-    let sum=0;
+    //O(1) space
+    let prefixsum=0,postfixsum=0;
     for(let i=1;i<=n;i++){
-        sum+=i;
-        prefix.push(sum);
+        prefixsum+=i;
     }
-    sum=0;
     for(let i=n;i>=1;i--){
-        sum+=i;
-        postfix.push(sum);
-    }
-    for(let i=0;i<n;i++){
-        if(prefix[i]===postfix[n-i-1])return i+1;
+        postfixsum+=i;
+        if(prefixsum===postfixsum)return i;
+        prefixsum-=i;
     }
     return -1;
 };
