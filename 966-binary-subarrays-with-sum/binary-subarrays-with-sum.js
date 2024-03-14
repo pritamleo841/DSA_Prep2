a7540f43333996e1e7c1b735a9c7cf6adabf505a
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-14 17:01:41 +0530

    Time: 68 ms (67.48%) | Memory: 52.8 MB (58.28%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
@@ -17,24 +17,37 @@ var numSubarraysWithSum = function(nums, goal) {
     // }
     // return count;
     //Two -pointer method
-    let count=0,i=0,res=0;
-    for(let j=0;j<nums.length;j++){
-        if(nums[j]===1){
-            goal--;
-            count=0;
-        }
-        while(goal===0 && i<=j){
-            goal+=nums[i];
-            i++;
-            count++;
-            if(i>j-goal+1)break;
-        }
-        while(goal<0){
-            goal+=nums[i];
-            i++;
-        }
-        res+=count;
+    // let count=0,i=0,res=0;
+    // for(let j=0;j<nums.length;j++){
+    //     if(nums[j]===1){
+    //         goal--;
+    //         count=0;
+    //     }
+    //     while(goal===0 && i<=j){
+    //         goal+=nums[i];
+    //         i++;
+    //         count++;
+    //         if(i>j-goal+1)break;
+    //     }
+    //     while(goal<0){
+    //         goal+=nums[i];
+    //         i++;
+    //     }
+    //     res+=count;
+    // }
+    // return res;
+    //Presum method
+    if(nums === null || nums.length == 0) return 0;
+    const freq = new Array(nums.length + 1).fill(0);
+    let count = 0;
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        let index = sum - goal;
+        if(index >= 0) count += freq[index];
+        if(sum == goal) count ++;
+        freq[sum]++;
     }
-    return res;
+    return count;
 
 };
