80ea1dd47e478cfeff8ac5c83311044753e16f33
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-14 17:19:11 +0530

    Time: 76 ms (69.96%) | Memory: 68 MB (68.26%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    let count=0;
+    const dfs=(node)=>{
+        if(!node)return;
+        count++
+        if(node.left)dfs(node.left);
+        if(node.right)dfs(node.right);
+    }
+    dfs(root);
+    return count;
+};
