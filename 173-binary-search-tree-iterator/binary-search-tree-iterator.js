c804b8965f66a535710330002281d1566719d32b
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-14 17:14:11 +0530

    Time: 124 ms (10.23%) | Memory: 63.6 MB (30.52%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.js
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.js
@@ -10,34 +10,44 @@
  * @param {TreeNode} root
  */
 var BSTIterator = function(root) {
-    if(!root){
-        this.data=[];
-        return;
-    }
-    //In Order DFS traversal [Sorted in low to high value]
-    let visited=[];
-    function traverse(node){
-        if(node.left)traverse(node.left);
-        visited.push(node.val);
-        if(node.right)traverse(node.right);
-    }
-    traverse(root);
-    //Reverse traversed result to have sorting from high to low value
-    this.data=visited.reverse();
+    // if(!root){
+    //     this.data=[];
+    //     return;
+    // }
+    // //In Order DFS traversal [Sorted in low to high value]
+    // let visited=[];
+    // function traverse(node){
+    //     if(node.left)traverse(node.left);
+    //     visited.push(node.val);
+    //     if(node.right)traverse(node.right);
+    // }
+    // traverse(root);
+    // //Reverse traversed result to have sorting from high to low value
+    // this.data=visited.reverse();
+    this.root=root;
+    this.stack=[];
 };
 
 /**
  * @return {number}
  */
 BSTIterator.prototype.next = function() {
-    return this.data.pop(); //O(1)
+    // return this.data.pop(); //O(1)
+    while(this.root){
+        this.stack.push(this.root);
+        this.root=this.root.left;
+    }
+    const res=this.stack.pop();
+    this.root=res.right;
+    return res.val;
 };
 
 /**
  * @return {boolean}
  */
 BSTIterator.prototype.hasNext = function() {
-    return this.data.length>0;
+    // return this.data.length>0;
+    return this.root || this.stack.length;
 };
 
 /** 
