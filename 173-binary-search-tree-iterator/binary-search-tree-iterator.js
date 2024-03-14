ff4f6d54f489daf6a78f0b1588acfb35176d584f
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-14 17:11:49 +0530

    Time: 120 ms (20.13%) | Memory: 63.5 MB (44.97%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+    if(!root){
+        this.data=[];
+        return;
+    }
+    //In Order DFS traversal [Sorted in low to high value]
+    let visited=[];
+    function traverse(node){
+        if(node.left)traverse(node.left);
+        visited.push(node.val);
+        if(node.right)traverse(node.right);
+    }
+    traverse(root);
+    //Reverse traversed result to have sorting from high to low value
+    this.data=visited.reverse();
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+    return this.data.pop(); //O(1)
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    return this.data.length>0;
+};
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
