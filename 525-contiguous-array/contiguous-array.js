df4796652c9b5d708fec42bb6374bfc8fe5a5a6a
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-16 12:58:19 +0530

    Time: 84 ms (79.44%) | Memory: 56.9 MB (65.37%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) {
+    /**
+    To find the maximum length, we need a dict to store the value of count (as the key) 
+    and its associated index (as the value). 
+    We only need to save a count value and its index at the first time, 
+    when the same count values appear again, 
+    we use the new index subtracting the old index to calculate the length of a subarray.
+    A variable max_length is used to to keep track of the current maximum length.
+    */
+    let count=0;
+    let maxLen=0;
+    let map=new Map();
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===0)count--;
+        if(nums[i]===1)count++;
+        if(count===0)maxLen=i+1; //contigous array of 0's and 1's
+        if(map.has(count)){
+            maxLen=Math.max(maxLen,i-map.get(count));
+        }else{
+            map.set(count,i)
+        }
+    }
+    return maxLen;
+};
