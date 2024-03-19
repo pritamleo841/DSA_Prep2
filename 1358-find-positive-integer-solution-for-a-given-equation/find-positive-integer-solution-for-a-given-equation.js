15ff484df3e17546b9856bf3e403d47d7abf0b87
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-20 00:08:28 +0530

    Time: 63 ms (53.33%) | Memory: 50.2 MB (43.33%) - LeetSync

--- 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.js
+++ 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.js
@@ -26,20 +26,29 @@ var findSolution = function (customfunction, z) {
     //     }
     // }
     // return ret;
+    //BINARY SEARCH
+    // const ret = [];
+    // let max = 1000;
+    // for (let x = 1; x <= 1000; ++x) {
+    //     if (max === 1) break;
+    //     for (let left = 1, right = max; left < right;) {
+    //         const mid = Math.floor((right + left) / 2);
+    //         const val = customfunction.f(x, mid);
+    //         if (val === z) {
+    //             ret.push([x, mid]);
+    //             max = mid;
+    //             break;
+    //         }
+    //         val < z ? (left = mid + 1) : (right = mid, max = mid);
+    //     }
+    // }
+    // return ret;
+    //TWO-POINTERS
     const ret = [];
-    let max = 1000;
-    for (let x = 1; x <= 1000; ++x) {
-        if (max === 1) break;
-        for (let left = 1, right = max; left < right;) {
-            const mid = Math.floor((right + left) / 2);
-            const val = customfunction.f(x, mid);
-            if (val === z) {
-                ret.push([x, mid]);
-                max = mid;
-                break;
-            }
-            val < z ? (left = mid + 1) : (right = mid, max = mid);
-        }
+    for (let x = 1, y = 1000; x <= 1000 && y >= 1;) {
+        const val = customfunction.f(x, y);
+        val === z && ret.push([x, y]);
+        val < z ? ++x : --y;
     }
     return ret;
 };
