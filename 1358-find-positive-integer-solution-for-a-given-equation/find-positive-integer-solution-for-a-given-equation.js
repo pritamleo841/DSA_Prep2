3a0b40e5c638de1270939a23059f6e0e664b64de
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-20 00:06:09 +0530

    Time: 59 ms (71.67%) | Memory: 49.6 MB (78.33%) - LeetSync

--- 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.js
+++ 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.js
@@ -15,14 +15,30 @@
  * @param {integer} z
  * @return {integer[][]}
  */
-var findSolution = function(customfunction, z) {
+var findSolution = function (customfunction, z) {
+    // const ret = [];
+    // for(let x=1;x<=1000;x++){
+    //     for(let y=1;y<=1000;y++){
+    //         if(customfunction.f(x,y)===z){
+    //             ret.push([x,y]);
+    //             break;
+    //         }
+    //     }
+    // }
+    // return ret;
     const ret = [];
-    for(let x=1;x<=1000;x++){
-        for(let y=1;y<=1000;y++){
-            if(customfunction.f(x,y)===z){
-                ret.push([x,y]);
+    let max = 1000;
+    for (let x = 1; x <= 1000; ++x) {
+        if (max === 1) break;
+        for (let left = 1, right = max; left < right;) {
+            const mid = Math.floor((right + left) / 2);
+            const val = customfunction.f(x, mid);
+            if (val === z) {
+                ret.push([x, mid]);
+                max = mid;
                 break;
             }
+            val < z ? (left = mid + 1) : (right = mid, max = mid);
         }
     }
     return ret;
