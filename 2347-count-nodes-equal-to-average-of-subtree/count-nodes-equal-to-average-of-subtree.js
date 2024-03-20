82351ccce1a54ce5a84760bc2e8f4e3b06c3c669
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-20 20:43:24 +0530

    Time: 71 ms (37.58%) | Memory: 53.9 MB (45.10%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.js
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.js
@@ -11,31 +11,51 @@
  * @return {number}
  */
 var averageOfSubtree = function(root) {
-    const SubtreeAvg = (node)=>{
-        let queue=[];
-        let sum=0,count=0;
-        queue.push(node);
-        while(queue.length>0){
-            const curr = queue.shift();
-            sum+=curr.val;
-            count++;
-            if(curr.left)queue.push(curr.left);
-            if(curr.right)queue.push(curr.right);
-        }
-        return Math.floor(sum/count);
-    };
+  
+    //using level order - O(n2)
+    // const SubtreeAvg = (node)=>{
+    //     let queue=[];
+    //     let sum=0,count=0;
+    //     queue.push(node);
+    //     while(queue.length>0){
+    //         const curr = queue.shift();
+    //         sum+=curr.val;
+    //         count++;
+    //         if(curr.left)queue.push(curr.left);
+    //         if(curr.right)queue.push(curr.right);
+    //     }
+    //     return Math.floor(sum/count);
+    // };
 
-    let count=0;
-    let queue=[];
-    queue.push(root);
-    while(queue.length>0){
-        const curr = queue.shift();
-        let avg = SubtreeAvg(curr);
-        if(avg===curr.val){
-            count++;
-        }
-        if(curr.left)queue.push(curr.left);
-        if(curr.right)queue.push(curr.right);
-    }
-    return count;
+    // let count=0;
+    // let queue=[];
+    // queue.push(root);
+    // while(queue.length>0){
+    //     const curr = queue.shift();
+    //     let avg = SubtreeAvg(curr);
+    //     if(avg===curr.val){
+    //         count++;
+    //     }
+    //     if(curr.left)queue.push(curr.left);
+    //     if(curr.right)queue.push(curr.right);
+    // }
+    // return count;
+
+    //we can optimize the solution using postorder traversal
+    let result = 0;
+    const traverse = (node) => { //postorder traversal
+        if (!node) return [0, 0];
+        const [leftSum, leftCount] = traverse(node.left);
+        const [rightSum, rightCount] = traverse(node.right);
+
+        const currSum = node.val + leftSum + rightSum;
+        const currCount = 1 + leftCount + rightCount;
+        
+        if (Math.floor(currSum/currCount) === node.val)result++;
+        
+        return [currSum, currCount];
+    };
+    
+    traverse(root);
+    return result;
 };
