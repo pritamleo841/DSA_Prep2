b1879cf667be8478087c5e65993c93c887f8ce28
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-03-28 20:09:32 +0530

    Time: 146 ms (84.07%) | Memory: 63.8 MB (92.92%) - LeetSync

---
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function(nums, k) {
+    let longest=0;
+    let start=0;
+    let freq={0:0};
+    for(let end=0;end<nums.length;end++){
+        freq[nums[end]] = (freq[nums[end]] || 0)+1;
+        while(freq[nums[end]]>k){
+            freq[nums[start]]--;
+            start++;
+        }
+        longest=Math.max(longest,end-start+1); //store max subarray size
+    }
+    return longest;
+};
