9fb21ac8f913b7863ccb6da58781bc029ece6f37
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-04-02 18:46:00 +0530

    Time: 57 ms (76.50%) | Memory: 50.6 MB (55.15%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+
+    let charMap = new Map();
+    let revCharMap = new Map();
+
+    if (s.length != t.length)
+        return false;
+
+    for (let i = 0; i < s.length; i++) {
+        let first = s[i];
+        let second = t[i];
+
+        if(
+            (charMap.has(first) && charMap.get(first) != second) ||
+            (revCharMap.has(second) && revCharMap.get(second) != first)
+        )return false;
+
+        charMap.set(first, second);
+        revCharMap.set(second, first);
+    }
+    return true;
+
+};
