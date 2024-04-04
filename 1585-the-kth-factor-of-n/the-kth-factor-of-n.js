a90ecf570d65bc55797ec474dbd63445fdc6aeaf
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-04-04 14:22:59 +0530

    Time: 63 ms (17.19%) | Memory: 49.1 MB (29.27%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kthFactor = function(n, k) {
+    let factors=[];
+    for(let i=1;i<=n;i++){
+        if(n%i===0)factors.push(i);
+    }
+    return factors.length>=k?factors[k-1]:-1;
+
+};
