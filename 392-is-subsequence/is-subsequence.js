ee815b1a850951f58a612034405f6034bb7a52ca
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-05-08 23:09:16 +0530

    Time: 67 ms (5.09%) | Memory: 51.8 MB (5.38%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,13 +4,16 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    if(s=="" && t=="")return true;
-    let ptr=0;
-    for(let char of t){
-        if(char === s[ptr]){
-            ptr++;
+
+    if(s.length==0 && t.length==0)return true;
+    //take an index and check consequitive elements of s in t
+    let index = 0;
+    for(let c of t){
+        if(c===s[index]){
+            index++;
         }
-        if(ptr===s.length)return true;
+        if(index===s.length)return true;
     }
     return false;
+
 };
