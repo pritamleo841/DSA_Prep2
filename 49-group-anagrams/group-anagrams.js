32b892790d530b2a5cf1791744f1e1b30230a8d1
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-05-09 22:21:27 +0530

    Time: 124 ms (39.51%) | Memory: 62 MB (82.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,27 +3,17 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let map = new Map();
-    let tempStrs = [...strs];
-    //Sort the string elements of temp array
-    for(let i=0;i<tempStrs.length;i++){
-        tempStrs[i]=sortString(tempStrs[i]);
+    
+    //sort each word in strs array
+    const sortedStr = strs.map(s=>s.split('').sort().join(''));
+    let map = {};
+    for(let i=0;i<strs.length;i++){
+        if(!map[sortedStr[i]]){
+            map[sortedStr[i]]=[strs[i]];
+        }else{
+            map[sortedStr[i]].push(strs[i]);
+        }
     }
-    //Map unique string values wrt sorted keys
-    for(let i=0;i<tempStrs.length;i++){
-        map.set(tempStrs[i],map.get(tempStrs[i])?map.get(tempStrs[i]).concat(strs[i]):[].concat(strs[i]));
-    }
-    //Use map iterator to push map key values into result array
-    const mapItr = map.values();
-    let i=0;
-    let result = [];
-    while(i<map.size){
-        result.push(mapItr.next().value);
-        i++;
-    }
-    return result;
-
+    //return array of object values
+    return Object.values(map);
 };
-var sortString = (str) =>{
-    return str.split('').sort().join('');
-}
