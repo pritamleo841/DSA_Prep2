bf826d93d1f94c497ca42e12a08c4af9409fe498
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-05-14 00:15:20 +0530

    Time: 54 ms (85.69%) | Memory: 51.4 MB (63.44%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,32 +3,16 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    //Using Map Datastructure - T:O(n)+S:O(n)
-    // let map=new Map();
-    // for(let i=0;i<nums.length;i++){
-    //     map.set(nums[i],(map.get(nums[i]) || 0)+1);
-    // }
-    // for(let [key,value] of map){
-    //     if(value>Math.floor(nums.length/2))return key;
-    // }
-
-    //Moore Voting Algorithm
-    /****
-    The intuition behind the Moore's Voting Algorithm is based on the fact that 
-    if there is a majority element in an array, it will always remain in the lead, 
-    even after encountering other elements.
-    The algorithm works on the basis of the assumption that 
-    the majority element occurs more than n/2 times in the array.
-    */
+    //Moore's voting algorithm
     let count=0;
-    let candidate=0;
+    let candidate=nums[0];
     for(let i=0;i<nums.length;i++){
-        if(count===0){ //if count=0, candidate=current_element
+        if(count===0){
             candidate=nums[i];
-            count=1; //set count=1, for new candidate
+            count++;
         }
-        else if(candidate===nums[i])count++; //if candidate=current_element, count++
-        else if(candidate!==nums[i])count--; //if candidate!=current_element, count--
+        else if(candidate===nums[i])count++;
+        else if(candidate!==nums[i])count--;
     }
-    return candidate;   //return candidate
+    return candidate;
 };
