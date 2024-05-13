d9d6c79770b8d7a759b4983da278a237dd06bf94
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-05-14 00:09:50 +0530

    Time: 631 ms (18.96%) | Memory: 71.9 MB (51.22%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -3,5 +3,29 @@
  * @return {number[]}
  */
 var sortArray = function (nums) {
-    return nums.sort((a,b)=>a-b);
+    //return nums.sort((a,b)=>a-b);
+    const quickSort = (start,end)=>{
+        if(start>=end)return;
+
+        let [left,right]=[start,end];
+        //taking middle element as pivot
+        let pivot = nums[Math.floor((start+end)/2)];
+        while(left<=right){
+            while(left<=right && nums[left]<pivot)left++;
+            while(left<=right && nums[right]>pivot)right--;
+            if(left<=right){
+                let temp = nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right--;
+            }
+        }
+        quickSort(start,right);
+        quickSort(left,end);
+    };
+    if(nums.length===1)return nums;
+    quickSort(0,nums.length-1);
+    return nums;
 };
+
