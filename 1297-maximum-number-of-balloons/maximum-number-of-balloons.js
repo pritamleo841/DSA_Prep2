f482efd83bd40644f6791d3d2854767a183ffbed
Pritam Sharma <sharmapritam841@gmail.com>
2024-05-21 23:40:20 +0530

    Time: 66 ms (29.37%) | Memory: 51.7 MB (33.96%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    let map = new Map();
+    for(let s of text){
+        map.set(s,(map.get(s) || 0)+1);
+    }
+    return Math.floor(
+        Math.min(map.get('b'),map.get('a'),map.get('l')/2,map.get('o')/2,map.get('n')) || 0
+    );
+};
