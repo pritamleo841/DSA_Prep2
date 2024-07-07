b2de0ff5fa3a3cb6ad7278b44bb437fffab62c3d
Pritam Sharma <sharmapritam841@gmail.com>
2024-07-07 17:05:15 +0530

    Time: 47 ms (83.74%) | Memory: 49.4 MB (27.67%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -3,19 +3,10 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    //Transpose the matrix
+    matrix.reverse();
     for(let i=0;i<matrix.length;i++){
-        for(let j=i;j<matrix[0].length;j++){
-            [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]]
+        for(let j=i+1;j<matrix[i].length;j++){
+            [matrix[i][j],matrix[j][i]]=[matrix[j][i],matrix[i][j]];
         }
-    }
-    //Reverse each row
-    // for(let i=0;i<matrix.length;i++){
-    //     for(let j=0;j<matrix[0].length/2;j++){
-    //         [matrix[i][j],matrix[i][matrix[0].length-j-1]]=[matrix[i][matrix[0].length-j-1],matrix[i][j]]
-    //     }
-    // }
-    for(let row of matrix){
-        row.reverse();
     }
 };
