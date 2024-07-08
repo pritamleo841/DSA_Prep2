da4118aae754de604a8a7f46f47d961f7e13ad50
Pritam Sharma <sharmapritam841@gmail.com>
2024-07-08 20:53:49 +0530

    Time: 43 ms (95.73%) | Memory: 49.3 MB (13.43%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,26 +6,15 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let id=m+n-1;
-    let i=m-1,j=n-1;
-    //we have to start from reverse side of the array
-    while(i>=0 && j>=0){
-        if(nums1[i]<nums2[j]){
-            nums1[id--]=nums2[j--];
-        }
-        else if(nums1[i]>nums2[j]){
-            nums1[id--]=nums1[i--];
-        }
-        else{
-            nums1[id--]=nums2[j--];
-            nums1[id--]=nums1[i--];
-        }
-    }
-    while(i>=0){
-        nums1[id--]=nums1[i--];
-    }
+    let i = m-1;
+    let j = n-1;
+    let k = m+n-1;
+
     while(j>=0){
-        nums1[id--]=nums2[j--];
+        if(i>=0 && nums1[i]>nums2[j]){
+            nums1[k--]=nums1[i--];
+        }else{
+            nums1[k--]=nums2[j--];
+        }
     }
-    console.log(nums1);
 };
