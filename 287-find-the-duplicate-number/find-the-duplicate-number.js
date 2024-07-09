23a5d0ba3b76957147f60105cdcb9acdf0abfe53
Pritam Sharma <sharmapritam841@gmail.com>
2024-07-09 21:40:56 +0530

    Time: 75 ms (72.89%) | Memory: 57.5 MB (64.76%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,9 +3,26 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let set = new Set();
-    for(let n of nums){
-        if(set.has(n))return n;
-        set.add(n);
+    // let set = new Set();
+    // for(let n of nums){
+    //     if(set.has(n))return n;
+    //     set.add(n);
+    // }
+
+    //Without using SET - Slow-Fast pointer technique
+    let fast = 0;
+    let slow = 0;
+    while (true) {
+        fast = nums[nums[fast]];
+        slow = nums[slow];
+        if (fast === slow) {
+            break;
+        }
     }
+    slow = 0;
+    while (nums[fast] !== nums[slow]) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return nums[fast];
 };
